b6146e6715dc60e741dbb792e414010dfc6bc8c4
Julio Antonio Ortiz Pol <j.ortiz@nexus.com.do>
2021-09-05 15:46:35 -0400

    create index.js with home route

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+const routes = {
+    "posts": "/posts/"
+  }
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.set("json spaces", 2);
+
+app.get("/", (request, response) => {
+    response.json({
+      info: "TCIT Post Api",
+      routes: routes,
+    });
+  });
